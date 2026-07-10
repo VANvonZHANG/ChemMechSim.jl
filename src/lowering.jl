8554e08f7e4bccabd3455efd7a86e62a25fe5c5e
Fan Zhang <vanvonzhang@gmail.com>
2026-07-10 11:03:03 +0800

    refactor(lowering): peel thermo.jl (NASA7 symbolic, K_c, reverse-rate policies)

--- src/lowering.jl
+++ src/lowering.jl
@@ -20,4 +20,5 @@ using DynamicQuantities     # u"..." unit literals used across the lowering file
 include("lowering/units.jl")
 include("lowering/state.jl")
 include("lowering/kinetics.jl")
+include("lowering/thermo.jl")
 include("lowering/core.jl")
